61dd13c6a12982d450cf26bc6d649d29679bbcef
Ibro <ibro@ibro.me>
2021-05-27 13:38:59 +0200

    Even more minimal

--- Program.cs
+++ Program.cs
@@ -1,15 +1,8 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 await using var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.MapGet("/", (Func<string>)(() => "Hello World HERE!"));
+app.MapGet("/", (System.Func<string>)(() => "Hello World HERE!"));
 
 await app.RunAsync();
